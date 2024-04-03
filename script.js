47f10e921d653d1c9d487d94477b30c5e390497d
Shina Cao <142919735+SheenaCao@users.noreply.github.com>
2024-04-03 06:52:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+document.getElementById('survey-form').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+    
+    alert('Thank you for your feedback!'); 
+});
+</script>
